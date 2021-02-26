2e308c320458b3d93389c5728d47e9f73ed37e19
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-02-26 23:51:06 +0500

    add
    add

---
+++ src/components/geoMap/GeoMap.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+export default function GeoMap() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginVertical: 30,
+        // marginBottom: 30,
+        height: 300,
+        // backgroundColor: "red",
+      }}
+    >
+      <MapView
+        style={{ flex: 1 }}
+        region={{
+          latitude: 42.882004,
+          longitude: 74.582748,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -14,6 +14,7 @@ import ProductCard from "../../components/productCard/ProductCard";
 import DropDown from "../../components/dropDown/DropDown";
 import HeaderText from "../../components/headerText/HeaderText";
 import CarouselItem from "../../components/carouselItem/CarouselItem";
+import GeoMap from "../../components/geoMap/GeoMap";
 
 export default function Home(props) {
   const { navigation } = props;
@@ -98,6 +99,8 @@ export default function Home(props) {
           <ProductCard key="4" navigation={navigation} />
           <ProductCard key="5" navigation={navigation} />
         </View>
+        <HeaderText />
+        <GeoMap />
       </ScrollView>
     </SafeAreaView>
   );
