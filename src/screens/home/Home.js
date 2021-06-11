7b24fbab6c7a5893f726d6972ad574547fb26ba9
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-06-11 17:56:54 +0500

    add
    add

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -9,81 +9,76 @@ import CarouselItem from "../../components/carouselItem/CarouselItem";
 import GeoMap from "../../components/geoMap/GeoMap";
 import { SetToken } from "../../store/Token/SetToken";
 import * as Animatable from "react-native-animatable";
-export default function Home ( props ) {
+export default function Home(props) {
   const { navigation } = props;
 
-  useEffect( () => {
+  useEffect(() => {
     SetToken();
-  } );
+  });
 
-  const [ state, setState ] = useState( {
+  const [state, setState] = useState({
     activeIndex: 0,
     carouselItems: [
       {
-        title: "Item 1",
-        text: "Text 1",
+        title: "Vелосипед 98 831,17 руб",
+        text: "Электрический велосипед 1000W мужские горный велосипед амортизационная вилка для велосипеда складной электровелосипед MX01 Электрический велосипед для взрослых с толстыми покрышками Байк, способный преодолевать Броды 48V литиевая Батарея",
+        img_src: "../../../assets/ae9fa23b8b74f7539aaadc90782ca53e.jpg",
       },
       {
-        title: "Item 2",
-        text: "Text 2",
+        title: "USB флеш-накопитель 196,13 - 994,00 руб ",
+        text: "USB флеш-накопитель TECHKEY, водонепроницаемый флеш-накопитель 64 ГБ 32 ГБ 16 ГБ 8 ГБ 4 ГБ, серебристая флешка, U-диск, подарок",
+        img_src: "../../../assets/ae9fa23b8b74f7539aaadc90782ca53e.jpg",
       },
       {
         title: "Item 3",
         text: "Text 3",
-      },
-      {
-        title: "Item 4",
-        text: "Text 4",
-      },
-      {
-        title: "Item 5",
-        text: "Text 5",
+        img_src: "../../../assets/ae9fa23b8b74f7539aaadc90782ca53e.jpg",
       },
     ],
-  } );
+  });
 
   return (
-    <SafeAreaView style={ { flex: 1 } }>
-      <TopHeader navigation={ navigation } isHome={ true } title="Shoppers" />
-      <ScrollView style={ { padding: 10 } }>
+    <SafeAreaView style={{ flex: 1 }}>
+      <TopHeader navigation={navigation} isHome={true} title="Shoppers" />
+      <ScrollView style={{ padding: 10 }}>
         <Animatable.View animation="fadeInUp">
           <HeaderText />
         </Animatable.View>
         <View
-          style={ {
+          style={{
             flexDirection: "row",
             paddingVertical: 16,
             flex: 1,
-          } }
+          }}
         >
           <Carousel
-            layout={ "default" }
-            data={ state.carouselItems }
-            sliderWidth={ 300 }
-            itemWidth={ 300 }
-            renderItem={ CarouselItem }
-            onSnapToItem={ ( index ) => setState( { ...state, activeIndex: index } ) }
+            layout={"default"}
+            data={state.carouselItems}
+            sliderWidth={300}
+            itemWidth={300}
+            renderItem={CarouselItem}
+            onSnapToItem={(index) => setState({ ...state, activeIndex: index })}
           />
         </View>
         <View
-          style={ {
+          style={{
             borderColor: "transparent",
             borderRightWidth: 0,
             borderLeftWidth: 0,
             borderBottomColor: "lightgrey",
             borderWidth: 2,
-          } }
+          }}
         >
           <HeaderText />
 
           <View
-            style={ {
+            style={{
               flex: 1,
               flexDirection: "row",
               justifyContent: "flex-end",
               alignContent: "center",
               alignItems: "center",
-            } }
+            }}
           >
             <View>
               <DropDown />
@@ -91,12 +86,12 @@ export default function Home ( props ) {
           </View>
         </View>
 
-        <View style={ { paddingBottom: 30 } }>
-          <ProductCard key="1" navigation={ navigation } />
-          <ProductCard key="2" navigation={ navigation } />
-          <ProductCard key="3" navigation={ navigation } />
-          <ProductCard key="4" navigation={ navigation } />
-          <ProductCard key="5" navigation={ navigation } />
+        <View style={{ paddingBottom: 30 }}>
+          <ProductCard key="1" navigation={navigation} />
+          <ProductCard key="2" navigation={navigation} />
+          <ProductCard key="3" navigation={navigation} />
+          <ProductCard key="4" navigation={navigation} />
+          <ProductCard key="5" navigation={navigation} />
         </View>
         <HeaderText title="map" subTitle="gogo map" />
         <GeoMap />
@@ -105,4 +100,4 @@ export default function Home ( props ) {
   );
 }
 
-const styles = StyleSheet.create( {} );
+const styles = StyleSheet.create({});
